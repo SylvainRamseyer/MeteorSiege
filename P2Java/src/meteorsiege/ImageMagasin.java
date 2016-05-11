61262cce9dfecb562cf46db04e8b06d0969f3303
sylvain.ramseyer <sylvain.ramseyer@he-arc.ch>
2016-05-11 16:05:11 +0200

    création d'une classe contenant tout les images sous forme statique

---
+++ P2Java/src/meteorsiege/ImageMagasin.java
@@ -0,0 +1,36 @@
+
+package meteorsiege;
+
+import org.newdawn.slick.Image;
+
+/**
+* Les images doivent se trouver dans un jar, et le jar dans le classpth!
+* Le jar doit contenir le folder ressources. A l'interieur du folder ressource doit se trouver les images aux formats (jpg, voir mieux png pour la transparance)
+*/
+public class ImageMagasin
+	{
+
+	/*------------------------------------------------------------------*\
+	|*		 Version Synchrone (bloquant)								*|
+	\*------------------------------------------------------------------*/
+	// station
+	public static final Image baseStation = Tools.loadImage("res/ufoRed.png");
+	public static final Image turret = Tools.loadImage("res/Parts/gun00.png");
+	public static final Image turretTop = Tools.loadImage("res/Parts/turretBase_small.png");
+
+	// project
+	public static final Image laserBlue = Tools.loadImage("res/Lasers/LaserBlue07.png");
+
+	// ennemis
+	public static final Image meteorBrown = Tools.loadImage("res/Meteors/meteorBrown_big1.png");
+
+
+	/*------------------------------------------------------------------*\
+	|*		Version Assynchrone	(non bloquant)							*|
+	\*------------------------------------------------------------------*/
+
+	//public static final ImageIcon warning = ImageLoader.loadAsynchroneJar("ressources/warning.png");
+	//public static final ImageIcon coffee = ImageLoader.loadAsynchroneJar("ressources/coffee_logo.png");
+	//public static final ImageIcon linux = ImageLoader.loadAsynchroneJar("ressources/linux.png");
+
+	}
